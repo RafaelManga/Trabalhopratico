3703a7a5c38784054f60d2ad37a4888bcbc7a63e
Almeida07 <mateusalmeidaseidel0732@gmail.com>
2026-04-02 20:23:22 -0300

    Update erros.h

--- erros.h
+++ erros.h
@@ -2,12 +2,15 @@
 #define ERROS_H
 
 /* Registra um erro léxico com detalhes, linha e coluna */
+
 void registrarErro(const char *tipo, int linha, int coluna, const char *detalhe);
 
 /* Salva a lista de erros no arquivo especificado */
+
 void salvarErros(const char *nomeArquivo);
 
 /* Retorna o total de erros encontrados até o momento */
+
 int obterTotalErros();
 
 #endif
