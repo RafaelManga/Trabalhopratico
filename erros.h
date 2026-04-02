a3caddd69ce2a8dc81afb82f34f12addc9b987b0
Almeida07 <mateusalmeidaseidel0732@gmail.com>
2026-04-02 20:19:53 -0300

    Update erros.h

--- erros.h
+++ erros.h
@@ -1,10 +1,13 @@
 #ifndef ERROS_H
 #define ERROS_H
 
-/* Registra um erro léxico encontrado durante a análise */
+/* Registra um erro léxico com detalhes, linha e coluna */
 void registrarErro(const char *tipo, int linha, int coluna, const char *detalhe);
 
-/* Salva todos os erros registrados no arquivo .err */
+/* Salva a lista de erros no arquivo especificado */
 void salvarErros(const char *nomeArquivo);
+
+/* Retorna o total de erros encontrados até o momento */
+int obterTotalErros();
 
 #endif
